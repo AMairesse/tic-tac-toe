216cc0d5d254e66a15d9de67ae29a1941b3da503
Antoine Mairesse <antoine.mairesse@free.fr>
2015-12-23 15:50:44 +0100

    Store and display each move for a game

--- tictactoe.py
+++ tictactoe.py
@@ -3,23 +3,23 @@
 
 # Use "print" in the Python3 way even if used with Python2
 from __future__ import print_function
-
+import copy
 
 class TicTacToe():
 	"""docstring for ClassName"""
 
 	# Public attribute
 	moves = 0
-	board = [[0,0,0],
-			 [0,0,0],
-			 [0,0,0]]
+	board = [[[0,0,0],
+			  [0,0,0],
+			  [0,0,0]]]
 
 	# Clear the board for a new game
 	def clear(self):
 		self.moves = 0
-		self.board = [[0,0,0],
-					  [0,0,0],
-					  [0,0,0]]
+		self.board = [[[0,0,0],
+					   [0,0,0],
+					   [0,0,0]]]
 
 	# Autoplay two AI and return :
 	#  - the winner
@@ -56,42 +56,55 @@ class TicTacToe():
 
 	# Make a move on the board
 	def play(self, sign, x, y):
+		# Take the last version of the board
+		new_move = copy.deepcopy(self.board[-1])
 		# Check first if the spot if free
-		if self.board[x][y] != 0:
+		if new_move[x][y] != 0:
 			return False
 		if sign == 'X':
-			self.board[x][y] = 1
+			new_move[x][y] = 1
 		elif sign == 'O':
-			self.board[x][y] = -1
+			new_move[x][y] = -1
 		else:
 			return False
 		self.moves = self.moves + 1
+		self.board.append(new_move)
 		return True
 
 	# Print the board on the screen
 	def display(self):
-		print("_______")
-		for x in range(3):
-			for y in range(3):
-				print ("|", end="")
-				if self.board[x][y] == 0:
-					print (" ", end="")
-				elif self.board[x][y] == 1:
-					print ("X", end="")
-				else:
-					print ("O", end="")
-			print ("|")
-		print("-------")
+		for board_number in xrange(1,self.board.__len__()):
+			print("_______  ", end="")
+		print("")
+
+		for x in xrange(3):
+			for board_number in xrange(1,self.board.__len__()):
+				for y in xrange(3):
+					print ("|", end="")
+					if self.board[board_number][x][y] == 0:
+						print (" ", end="")
+					elif self.board[board_number][x][y] == 1:
+						print ("X", end="")
+					else:
+						print ("O", end="")
+				print ("|  ", end="")
+			print("")
+
+		for board_number in xrange(1,self.board.__len__()):
+			print("-------  ", end="")
+		print("")
 
 	# Export the board in a single row and
 	# values between 0 and 1(for AI players)
 	def readBoard(self):
+		# Take the last version of the board
+		last_move = self.board[-1]
 		result = []
 		for x in xrange(0,3):
 			for y in xrange(0,3):
-				if self.board[x][y] == 0:
+				if last_move[x][y] == 0:
 					result.append(0)
-				elif self.board[x][y] == -1:
+				elif last_move[x][y] == -1:
 					result.append(0.5)
 				else:
 					result.append(1)
@@ -99,27 +112,29 @@ class TicTacToe():
 
 	# Check if there is a winner and return which sign won
 	def isThereWinner(self):
+		# Take the last version of the board
+		last_move = self.board[-1]
 		# Lines
 		for x in range(3):
-			line = self.board[x][0] + self.board[x][1] + self.board[x][2]
+			line = last_move[x][0] + last_move[x][1] + last_move[x][2]
 			if line == 3 :
 				return('X')
 			elif line == -3 :
 				return('O')
 		# Columns
 		for x in range(3):
-			column = self.board[0][x] + self.board[1][x] + self.board[2][x]
+			column = last_move[0][x] + last_move[1][x] + last_move[2][x]
 			if column == 3 :
 				return('X')
 			elif column == -3 :
 				return('O')
 		# Diagonals
-		diag = self.board[0][0] + self.board[1][1] + self.board[2][2]
+		diag = last_move[0][0] + last_move[1][1] + last_move[2][2]
 		if diag == 3 :
 			return('X')
 		elif diag == -3 :
 			return('O')
-		diag = self.board[2][0] + self.board[1][1] + self.board[0][2]
+		diag = last_move[2][0] + last_move[1][1] + last_move[0][2]
 		if diag == 3 :
 			return('X')
 		elif diag == -3 :
