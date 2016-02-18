a19c8d1a230baba1acec097d55ecd9666a4744e1
Antoine Mairesse <antoine.mairesse@free.fr>
2016-02-18 15:00:46 +0100

    Add unit tests for new features

--- tictactoe_test.py
+++ tictactoe_test.py
@@ -98,6 +98,34 @@ class TicTacToetestCase(unittest.TestCase):
 		self.game.play('O', 2, 2)
 		self.assertEqual(self.game.isThereWinner(), 'O')
 
+	def testFirstDiagWithX(self):
+		self.game.clear()
+		self.game.play('X', 0, 0)
+		self.game.play('X', 1, 1)
+		self.game.play('X', 2, 2)
+		self.assertEqual(self.game.isThereWinner(), 'X')
+
+	def testFirstDiagWithO(self):
+		self.game.clear()
+		self.game.play('O', 0, 0)
+		self.game.play('O', 1, 1)
+		self.game.play('O', 2, 2)
+		self.assertEqual(self.game.isThereWinner(), 'O')
+
+	def testSecondDiagWithX(self):
+		self.game.clear()
+		self.game.play('X', 0, 2)
+		self.game.play('X', 1, 1)
+		self.game.play('X', 2, 0)
+		self.assertEqual(self.game.isThereWinner(), 'X')
+
+	def testSecondDiagWithO(self):
+		self.game.clear()
+		self.game.play('O', 0, 2)
+		self.game.play('O', 1, 1)
+		self.game.play('O', 2, 0)
+		self.assertEqual(self.game.isThereWinner(), 'O')
+
 	def testPlayControls(self):
 		self.game.clear()
 		self.assertTrue(self.game.play('O', 0, 0))
@@ -108,6 +136,18 @@ class TicTacToetestCase(unittest.TestCase):
 		self.game.play('O', 0, 0)
 		self.game.play('X', 2, 2)
 		self.assertEqual(self.game.readBoard(), [[0.5, 0, 0, 0, 0, 0, 0, 0, 1]])
+
+	def testBestMove(self):
+		self.game.clear()
+		(x, y) = self.game.best_move('X')
+		self.assertEqual(x, 1)
+		self.assertEqual(y, 1)
+
+	def testScore(self):
+		self.game.clear()
+		self.game.play('X', 1, 1)
+		score = self.game.score(0,0,'O')
+		self.assertEqual(score, -0.8445357084274292)
 
 if __name__ == '__main__':
     unittest.main()
